ac4dcc4b4efcd288a7570328fbd03b40af0c9b96
Margaret Berry <codeberry1@gmail.com>
2017-12-17 01:08:35 -0800

    show login forms in dialog box

--- src/app/auth-dialog/auth-dialog.component.ts
+++ src/app/auth-dialog/auth-dialog.component.ts
@@ -13,9 +13,29 @@ export class AuthDialogComponent implements OnInit {
 
   constructor() { }
 
+  onLoginFormResult(e){
+   if(e.signedIn)
+     this.closeDialog();
+   else {
+     alert(e.err.json().errors[0])
+    }
+  }
+
+ onRegisterFormResult(e){
+   if(e.signedUp)
+     this.closeDialog();
+   else {
+     alert(e.err.json().errors.full_messages[0])
+    }
+  }
+
   openDialog(mode: 'login' | 'register' = 'login'){
     this.authMode = mode;
     this.modalActions.emit({action:"modal", params:['open']});
+  }
+
+ closeDialog(){
+    this.modalActions.emit({action:"modal", params:['close']});
   }
 
   ngOnInit() {
