18839ae7d16415e2f9cf741159318064fdc65c70
Margaret Berry <codeberry1@gmail.com>
2017-12-21 14:03:52 -0800

    remove unused code

--- src/app/services/web-api-observable.service.ts
+++ src/app/services/web-api-observable.service.ts
@@ -37,20 +37,4 @@ export class WebApiObservableService {
     })
   }
 
-  createService(url: string, param: any): Observable<any> {
-    let body = JSON.stringify(param);
-    return this.http.post(url, body, this.options).map(this.extractData).catch(this.handleError);
-  }
-
-  private extractData(res:Response) {
-    let body = res.json();
-    return body || {};
-  }
-
-  private handleError(error: any) {
-    let errMsg = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
-
 }
