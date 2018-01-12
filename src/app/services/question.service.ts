f11bb5305d2b98d3c215d1342e6d1675f766e303
Margaret Berry <codeberry1@gmail.com>
2018-01-12 12:22:12 -0800

    rename question service

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -3,6 +3,7 @@ import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { NewQuestion } from '../new-question.model';
 import { Angular2TokenService } from "angular2-token";
+import { ActivatedRoute, Params } from '@angular/router';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
@@ -16,16 +17,26 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 
 @Injectable()
-export class WebApiObservableService {
+export class QuestionService {
   questions: Observable<any>;
 
+  questionId: number = null;
+
   constructor(private questionService: Angular2TokenService) {
+
     this.questions = this.questionService.get('questions');
+
   }
 
   addQuestion(newQuestion:NewQuestion) {
     console.log(newQuestion);
     this.questionService.post('questions', newQuestion);
+  }
+
+  editQuestion(questionToEdit:NewQuestion) {
+    console.log(questionToEdit);
+    
+    this.questionService.patch(`questions/${this.questionId}`, questionToEdit);
   }
 
 }
