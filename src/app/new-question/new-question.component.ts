fbfd9eefa490f7641d7115fc0f808652b3bcc36b
Margaret Berry <codeberry1@gmail.com>
2018-01-12 12:23:02 -0800

    incorporate edit question component

--- src/app/new-question/new-question.component.ts
+++ src/app/new-question/new-question.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
-import { WebApiObservableService } from "../services/web-api-observable.service";
+import { QuestionService } from "../services/question.service";
 import { Http } from "@angular/http";
 import { Angular2TokenService } from "angular2-token";
 import { NewQuestion } from '../new-question.model';
@@ -10,13 +10,13 @@ import { NewQuestion } from '../new-question.model';
   selector: 'app-new-question',
   templateUrl: './new-question.component.html',
   styleUrls: ['./new-question.component.sass'],
-  providers: [ WebApiObservableService ]
+  providers: [ QuestionService ]
 })
 export class NewQuestionComponent implements OnInit {
 
 newQuestionModel = NewQuestion;
 
-  constructor(private questionService: WebApiObservableService) {
+  constructor(private questionService: QuestionService) {
   }
 
   ngOnInit() {
