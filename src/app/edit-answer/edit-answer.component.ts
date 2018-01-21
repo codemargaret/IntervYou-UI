8152af972e6c4dc5b9fe49bfd3c32e50f3625a8d
Margaret Berry <codeberry1@gmail.com>
2018-01-21 11:54:31 -0800

    create edit answer component

---
+++ src/app/edit-answer/edit-answer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-answer',
+  templateUrl: './edit-answer.component.html',
+  styleUrls: ['./edit-answer.component.sass']
+})
+export class EditAnswerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
