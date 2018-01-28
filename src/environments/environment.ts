852ec062aec31350bf1b877cc4be18606ccfd902
Margaret Berry <codeberry1@gmail.com>
2018-01-28 14:36:58 -0800

    remove api configuration to test for database issues with deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  token_auth_config: {
-    apiBase: 'http://localhost:3000'
-  }
+  production: false
+  // token_auth_config: {
+  //   apiBase: 'http://localhost:3000'
+  // }
 };
